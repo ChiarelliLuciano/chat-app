0f18047977b8fa9582fcb5b8afcd0c1282c39920
Luciano <luciano.miguel.ch95@gmail.com>
2023-05-28 12:12:27 -0300

    add users layout, sidebar component & hooks

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,6 +1,11 @@
-"use client";
-import { signOut } from "next-auth/react";
+import EmptyState from "../components/EmptyState";
 
-export default function Users() {
-  return <button onClick={() => signOut()}>Log Out</button>;
-}
+const Users = () => {
+  return (
+    <div className="hidden lg:block lg:pl-80 h-full">
+      <EmptyState />
+    </div>
+  );
+};
+
+export default Users;
