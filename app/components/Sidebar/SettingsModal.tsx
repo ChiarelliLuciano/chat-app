f2009e013e21592a049cd852fae2406537378ce0
Luciano <luciano.miguel.ch95@gmail.com>
2023-07-05 22:50:19 -0300

    modify modal

--- app/components/Sidebar/SettingsModal.tsx
+++ app/components/Sidebar/SettingsModal.tsx
@@ -5,6 +5,11 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
+import Modal from "../Modal";
+import Input from "../Input/Input";
+import Image from "next/image";
+import { CldUploadButton } from "next-cloudinary";
+import Button from "../Button";
 
 interface SettingsModalProps {
   isOpen?: boolean;
@@ -54,9 +59,64 @@ const SettingsModal: React.FC<SettingsModalProps> = ({
   };
 
   return (
-    <>
-      <div>Settings Modal</div>
-    </>
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="space-y-12">
+          <div className="border-b border-gray-900/10 pb-12">
+            <h2 className="text-base font-semibold leading-7 text-gray-900">
+              Perfil
+            </h2>
+            <p className="mt-1 leading-6 text-sm text-gray-500">
+              Aquí puedes editar tus datos públicos:
+            </p>
+            <div className="mt-10 flex flex-col gap-y-8">
+              <Input
+                disabled={isLoading}
+                label="Nombre"
+                id="name"
+                errors={errors}
+                required
+                register={register}
+              />
+              <div>
+                <label className="block text-sm font-medium leading-6 text-gray-900">
+                  Foto
+                </label>
+                <div className="mt-2 flex items-center gap-x-3">
+                  <Image
+                    width="48"
+                    height="48"
+                    className="rounded-full"
+                    src={
+                      image || currentUser?.image || "/images/placeholder.jpg"
+                    }
+                    alt="avatar"
+                  />
+                  <CldUploadButton
+                    options={{ maxFiles: 1 }}
+                    onUpload={handleUpload}
+                    uploadPreset="miv811o1"
+                  >
+                    <Button disabled={isLoading} secondary type="button">
+                      Cambiar
+                    </Button>
+                  </CldUploadButton>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-6 flex items-center justify-end gap-x-6">
+            <Button disabled={isLoading} secondary onClick={onClose}>
+              Cancelar
+            </Button>
+            <Button disabled={isLoading} type="submit">
+              Guardar
+            </Button>
+          </div>
+        </div>
+      </form>
+    </Modal>
   );
 };
 
